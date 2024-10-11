6d3d0977034b57b3f264077ef037b6054b2110db
jatinchavan <57410830+jatinchavan@users.noreply.github.com>
2024-10-11 13:04:21 +0100

    CountrySearch App - Add build script and configure AWS Amplify

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/",
   plugins: [react()],
   test: {
     environment: "jsdom",
     globals: true,
     setupFiles: "./src/setupTests.ts",
+  },
+  build: {
+    outDir: "dist",
   },
 });
